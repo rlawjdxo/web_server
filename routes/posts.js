fc5c05d0b4f45f67f2d649fe54497f6ac7cc0d11
rlawjdxo <rlawjd0721@naver.com>
2023-11-18 15:48:23 +0900

    v12

--- routes/posts.js
+++ routes/posts.js
@@ -11,5 +11,10 @@ router.get('/insert', function(req, res) {
     res.render('index', { title: '글쓰기', pageName:'posts/insert.ejs' });
 });
 
+/* posts 게시판 페이지 */
+router.get('/read', function(req, res, next) {
+    const id=req.query.id;
+    res.render('index', { title: '게시글정보', pageName:'posts/read.ejs', id });
+});
 
 module.exports = router;
